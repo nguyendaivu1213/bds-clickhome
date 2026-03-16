8daf124687a30a9a681b2cad9d9323583d0c2379
nguyendaivu1213 <nguyendaivu1213@gmail.com>
2026-03-16 08:44:12 +0700

    fix build

---
+++ admin/types/tiptap.d.ts
@@ -0,0 +1,4 @@
+declare module 'tiptap-extension-line-height';
+declare module 'tiptap-extension-font-size';
+declare module 'tiptap-extension-resize-image';
+declare module '@weiruo/tiptap-extension-indent';
