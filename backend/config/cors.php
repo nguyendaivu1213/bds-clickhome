90cd03a14287c0fa6538b0adf331cc6a8670a8a7
nguyendaivu1213 <nguyendaivu1213@gmail.com>
2026-03-17 22:15:17 +0700

    fix cross domain

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -7,7 +7,9 @@ return [
 
     'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001,http://localhost:3003,http://127.0.0.1:3003')),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https?://.*\.clickhomes\.vn',
+    ],
 
     'allowed_headers' => ['*'],
 
